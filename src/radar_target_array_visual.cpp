742542e0d504f70bc3671a2a1584b7ec4def5e13
Nick Rotella <nick.rotella@ainstein.ai>
2019-10-29 13:55:48 -0500

    Rebase from 'release/kinetic/ainstein_radar_rviz_plugins'

--- src/radar_target_array_visual.cpp
+++ src/radar_target_array_visual.cpp
@@ -61,6 +61,9 @@ RadarTargetArrayVisual::RadarTargetArrayVisual( Ogre::SceneManager* scene_manage
 
 RadarTargetArrayVisual::~RadarTargetArrayVisual()
 {
+  // Clear the visual:
+  radar_target_visuals_.clear();
+
   // Destroy the frame node since we don't need it anymore.
   scene_manager_->destroySceneNode( frame_node_ );
 }
@@ -68,7 +71,7 @@ RadarTargetArrayVisual::~RadarTargetArrayVisual()
 void RadarTargetArrayVisual::setMessage( const ainstein_radar_msgs::RadarTargetArray::ConstPtr& msg )
 {
   // Resize the target shapes vector:
-  radar_target_visuals_.clear();
+  clearMessage();
   
   // Fill the target shapes from RadarTargetArray message:
   for( const auto& target : msg->targets )

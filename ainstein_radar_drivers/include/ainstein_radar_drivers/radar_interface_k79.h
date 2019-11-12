a5a319a1b293af1c0f9a2abf37b04e1d49016f56
Nick Rotella <nick.rotella@ainstein.ai>
2019-11-12 16:00:17 -0600

    Rebase from 'upstream'

--- ainstein_radar_drivers/include/ainstein_radar_drivers/radar_interface_k79.h
+++ ainstein_radar_drivers/include/ainstein_radar_drivers/radar_interface_k79.h
@@ -7,9 +7,10 @@
 #include <memory>
 #include <thread>
 
-#include <ros/ros.h>
+#include <ainstein_radar_msgs/RadarInfo.h>
 #include <ainstein_radar_msgs/RadarTargetArray.h>
 #include <ainstein_radar_drivers/radar_driver_k79.h>
+#include <ros/ros.h>
 
 namespace ainstein_radar_drivers
 {
@@ -35,7 +36,38 @@ public:
     return target;
   }
   
+  // Radar specifications:
+  static constexpr double UPDATE_RATE = 10.0;
+  static constexpr int MAX_NUM_TARGETS = 1000;
+
+  static constexpr double RANGE_MIN = 0.0;
+  static constexpr double RANGE_MAX = 40.0;
+
+  static constexpr double SPEED_MIN = 0.0;
+  static constexpr double SPEED_MAX = 0.0;
+
+  static constexpr double AZIMUTH_MIN = -40.0;
+  static constexpr double AZIMUTH_MAX = 40.0;
+
+  static constexpr double ELEVATION_MIN = -4.0;
+  static constexpr double ELEVATION_MAX = 4.0;
+
+  static constexpr double RANGE_RES = 0.0;
+  static constexpr double RANGE_ACC = 0.1;
+
+  static constexpr double SPEED_RES = 0.0;
+  static constexpr double SPEED_ACC = 0.1;
+
+  static constexpr double AZIMUTH_RES = 0.0;
+  static constexpr double AZIMUTH_ACC = 1.0;
+
+  static constexpr double ELEVATION_RES = 0.0;
+  static constexpr double ELEVATION_ACC = 0.0;
+  
 private:
+
+  void publishRadarInfo( void );
+  
   std::string frame_id_;
 
   std::unique_ptr<ainstein_radar_drivers::RadarDriverK79> driver_;
@@ -47,8 +79,10 @@ private:
   ros::NodeHandle nh_;
   ros::NodeHandle nh_private_;
   ros::Publisher pub_radar_data_raw_;
+  ros::Publisher pub_radar_info_;
 
   boost::shared_ptr<ainstein_radar_msgs::RadarTargetArray> radar_data_msg_ptr_raw_;      
+  boost::shared_ptr<ainstein_radar_msgs::RadarInfo> radar_info_msg_ptr_;      
 };
 
 } // namespace ainstein_radar_drivers

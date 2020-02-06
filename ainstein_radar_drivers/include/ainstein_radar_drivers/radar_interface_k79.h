fe13356029580252a84b4c606153b54d223ef716
Nick Rotella <nick.rotella@ainstein.ai>
2020-02-06 12:51:02 -0600

    Rebase from 'upstream'

--- ainstein_radar_drivers/include/ainstein_radar_drivers/radar_interface_k79.h
+++ ainstein_radar_drivers/include/ainstein_radar_drivers/radar_interface_k79.h
@@ -41,7 +41,7 @@ public:
   static constexpr int MAX_NUM_TARGETS = 1000;
 
   static constexpr double RANGE_MIN = 0.0;
-  static constexpr double RANGE_MAX = 40.0;
+  static constexpr double RANGE_MAX = 120.0;
 
   static constexpr double SPEED_MIN = 0.0;
   static constexpr double SPEED_MAX = 0.0;
@@ -52,13 +52,13 @@ public:
   static constexpr double ELEVATION_MIN = -4.0;
   static constexpr double ELEVATION_MAX = 4.0;
 
-  static constexpr double RANGE_RES = 0.0;
+  static constexpr double RANGE_RES = 0.1;
   static constexpr double RANGE_ACC = 0.1;
 
-  static constexpr double SPEED_RES = 0.0;
+  static constexpr double SPEED_RES = 0.16;
   static constexpr double SPEED_ACC = 0.1;
 
-  static constexpr double AZIMUTH_RES = 0.0;
+  static constexpr double AZIMUTH_RES = 2.5;
   static constexpr double AZIMUTH_ACC = 1.0;
 
   static constexpr double ELEVATION_RES = 0.0;
